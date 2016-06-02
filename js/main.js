b3a5eda5603b8da7e385bf10152d4954f9f3bed3
braindots <dmadhankumarr@gmail.com>
2016-06-02 10:02:05 +0530

    icons updated
    home page banner and icons

--- js/main.js
+++ js/main.js
@@ -95,4 +95,28 @@ jQuery(function($) {'use strict',
 	//       }, 700);
 	 
 	// });
+
+// multiple tabs 
+
+      $('.tabs').each(function(){
+          var $active, $content, $links = $(this).find('a');
+          $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+          //$active.addClass('active');
+          $content = $($active[0].hash);
+          $links.not($active).each(function () {
+            $(this.hash).hide();
+          });
+
+          // Bind the click event handler
+          $(this).on('click', 'a', function(e){
+            $active.removeClass('active');
+            $content.hide();
+            $active = $(this);
+            $content = $(this.hash);
+            $active.addClass('active');
+            $content.show();
+            e.preventDefault();
+          });
+       });
+      
 });
